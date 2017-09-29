d46485f998503882b6b0b13c4ec1e8e56b9812ad
Lgodoy85 <31898581+Lgodoy85@users.noreply.github.com>
2017-09-28 21:02:22 -0300

    Iteracion while 3

---
+++ IteracionWhile2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iteracion.pkgwhile.pkg2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc
+ */
+public class IteracionWhile2 {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+Scanner Misc = new Scanner (System.in); 
+Integer micontador = 0;
+
+while (micontador<3)
+{
+    micontador=micontador+1;
+    System.out.println(" Contando.. ");
+    
+}
+        System.out.println(" Termine ");
+    }
+    
+}
