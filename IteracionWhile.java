9c3becf629e0fd0eded0959af6b522f52aae52fd
Lgodoy85 <31898581+Lgodoy85@users.noreply.github.com>
2017-10-16 23:49:21 -0300

    Update IteracionWhile.java

--- IteracionWhile.java
+++ IteracionWhile.java
@@ -115,3 +115,293 @@ System.out.println(" El promedio es :  " +promedio );
 
 
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iteracion.pkgwhile.pkg4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc
+ */
+public class IteracionWhile4 {
+// Pedir 5 edades e informar cuantos mayores a 18 hay. 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//Declaramos la variable y usamos el scanner
+
+String aux;
+Integer edad;
+Integer contador=0;
+Integer menores=0;
+Integer mayores=0;
+Scanner Misca = new Scanner (System.in);
+
+//creamos el while para 5 personas
+while (contador < 5)
+{
+    contador++; //sumamos el contador +1
+    
+    //pedimos datos y luego parseamos
+    System.out.println(" Ingrese la edad  " +contador );
+    aux = Misca.next();
+    edad = Integer.parseInt(aux);
+    
+    //creamos la condicion
+    if (edad>17)
+    {       mayores++; //el contador de mayores,mayores++ es (mayores=mayores+1); como un contador;
+    /*
+       System.out.println( " Mayores es igual a :  " +mayores); } **/
+    }
+    else
+        menores++;
+        
+}
+        System.out.println(" La cantidad de mayores a 18 años son :  " +mayores+ " y de menores son : " +menores );
+    }
+    
+    
+}
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iteracion.pkgwhile.pkg5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc
+ */
+public class IteracionWhile5 {
+
+//Pedir 5 numeros e informar cuantos positivos hay. 
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//Declaramos la vatiable y usamos el scanner
+Scanner Misca = new Scanner(System.in);
+String dato;
+Integer contador=0;
+Integer numeros;
+Integer positivos = 0;
+
+//usamos el while para el contador
+while ( contador < 5)
+{ contador++;   //incrementamos el contador +1
+//pedimos datos y parseamos
+System.out.println(" Ingrese el numero ");
+dato = Misca.next();
+numeros = Integer.parseInt(dato);
+//declaramos la sentencia
+if ( numeros >0) 
+{ positivos++;}
+}
+//mostramos la cantidad de positivos
+System.out.println(" Lc cantidad de numeros positivos son :  " +positivos);
+{
+}   
+}
+
+    
+   /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iteracion.pkgwhile.pkg6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc
+ */
+public class IteracionWhile6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//Declaramos la vatiable y usamos el scanner
+Scanner Misca = new Scanner (System.in);
+String dato;
+Integer contador = 0;
+Integer numero;
+Integer pares = 0;
+
+//usamos el while para el contador
+while (contador < 5)
+//incrementamos el contador
+{ contador++;
+
+//pedimos el dato y luego parseamos
+System.out.println(" Ingrese el numero ");
+dato = Misca.next();
+numero = Integer.parseInt(dato);
+
+//usamos el condicional para los pares, si da 0 es par , sino  es impar
+if ( numero % 2==0)
+{ pares++;
+}
+}
+//Mostramos el mensaje
+System.out.println(" La cantidad de numeros pares son :  " +pares);
+}
+}  
+
+    
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iteracion.pkgwhile.pkg7;
+
+import java.util.Scanner;
+
+  /*
+  pedir 5 nombres e informar cuantas marias hay.
+   */
+/**
+ *
+ * @author pc
+ */
+public class IteracionWhile7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// Declaramos variable y usamos el 
+Scanner Misca = new Scanner (System.in);
+String data;
+String nombres;
+Integer contador = 0;
+Integer cantidaddemarias = 0;
+
+//Usamos el while
+while ( contador < 5 )
+{ contador ++;
+
+ //mostramos el mensaje y luego parseamos
+System.out.println(" Ingrese 5 nombres ");
+nombres = Misca.next();
+
+//usamos el condicional para saber si nombres = a maria 
+if (nombres.equals("marias"))
+{ cantidaddemarias++;
+}
+}
+        System.out.println(" La cantidad de Marias es : " +cantidaddemarias  );
+    }}
+    
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iteracion.pkgwhile.pkg8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc
+ */
+public class IteracionWhile8 {
+//Calcular el promedio de edad
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// Declaramos la variable y usamos el scanner
+Scanner Misca = new Scanner (System.in);
+String dato;
+String respuesta;
+Integer edad;
+Integer sumaDeEdad = 0;
+Integer contador = 0;
+Integer promedio = 0;
+
+//mostramos el mensaje preguntando si hay alguien para la edad
+System.out.println(" Hay alguien para le edad ? ");
+respuesta = Misca.next();
+
+//usamoos el while para responder si o no
+while (respuesta.equals("si"))
+{
+//mostramos el mensaje y luego parseamos
+System.out.println(" Ingrese la edad ");
+dato = Misca.next();
+edad = Integer.parseInt(dato);
+
+//acumulo las edades
+sumaDeEdad = sumaDeEdad + edad;
+//incremento con el contador
+contador++;
+
+//vuelvo a preguntar si hay mas edad, si es no, cierra el while
+System.out.println(" Hay alguien mas para la edad ? ");
+respuesta = Misca.next();
+}
+
+//hago el promedio de edad
+promedio = sumaDeEdad / contador;
+//muestro el promedio
+System.out.println(" El promedio total es :  " +promedio);
+    }
+    
+}
+    
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iteracion.pkgwhile.pkg9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc
+ */
+public class IteracionWhile9 {
+//Mostrar numeros pares
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//Declaramos la variable y usamos el scanner
+Scanner Misca = new Scanner (System.in);
+Integer contador = 0;
+
+while (contador<100)
+{
+    contador++;
+    if (contador%2==0)
+        System.out.println(" Es par : " +contador);
+    }
+    }   
+
+}
+
+ 
+
+
+
