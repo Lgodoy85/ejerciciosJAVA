ffa49113925d30e6966206ae16a8b0909107df6e
Lgodoy85 <31898581+Lgodoy85@users.noreply.github.com>
2017-10-20 00:36:51 -0300

    Ejercicio 37

---
+++ Ejercicio37.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg37;
+
+/**
+ *
+ * @author pc
+ */
+public class Ejercicio37 {
+//Mostrar todos los numeros impares del 1 al 100.
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int contador = 1;
+    
+    while (contador < 100)
+    {
+        contador++;
+        System.out.println(" Numeros impares : " +contador);
+        contador++;
+        contador++;
+    }
+    }
+    
+}
