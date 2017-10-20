7a9cb7607dac2dddb8bede596260dcadb043a27f
Lgodoy85 <31898581+Lgodoy85@users.noreply.github.com>
2017-10-20 00:35:16 -0300

    Ejercicio 36

---
+++ Ejercicio36.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg36;
+
+/**
+ *
+ * @author pc
+ */
+public class Ejercicio36 {
+
+    //Mostrar todos los numeros pares del 1 al 100
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //mostramos el mensaje
+    
+    int contador = 1;
+    
+    while (contador<100)
+    {
+contador++; 
+        System.out.println(" Numeros pares : " +contador);
+                contador++;
+    }}
+    
+}
