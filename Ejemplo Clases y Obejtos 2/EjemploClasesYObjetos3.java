cbc0a5af335cb8b086072d040b980cec64fb3b08
Lgodoy85 <31898581+Lgodoy85@users.noreply.github.com>
2017-11-09 16:09:22 -0300

    Add files via upload

---
+++ Ejemplo Clases y Obejtos 2/EjemploClasesYObjetos3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploclasesyobjetos3;
+
+/**
+ *
+ * @author pc
+ */
+public class EjemploClasesYObjetos3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+        Lapicera lapicera=new Lapicera();
+        
+        
+        lapicera.escribir("azul", 80);
+        lapicera.cargarTinta("azul", 150);
+        
+    }
+    
+}   
