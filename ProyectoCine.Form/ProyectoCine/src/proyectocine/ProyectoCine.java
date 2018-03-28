435b4e0d3e3e85a697d99a57c3ae3e0c0fcf2792
Lgodoy85 <31898581+Lgodoy85@users.noreply.github.com>
2018-03-28 16:11:09 -0300

    Add files via upload

---
+++ ProyectoCine.Form/ProyectoCine/src/proyectocine/ProyectoCine.java
@@ -0,0 +1,20 @@
+package proyectocine;
+
+import FRcine.FormCine;
+
+/**
+ *
+ * @author alumno
+ */
+public class ProyectoCine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        FormCine fr = new FormCine();
+        fr.setVisible(true);
+    }
+    
+}
---
+++ ProyectoCine.Form/Visor de Imagenes mdi/src/visor/de/imagenes/mdi/VisorDeImagenesMDI.java
@@ -0,0 +1,13 @@
+package visor.de.imagenes.mdi;
+
+import Vistas.FRMInicioPrincipal;
+
+public class VisorDeImagenesMDI {
+
+    public static void main(String[] args) {
+
+        FRMInicioPrincipal FR = new FRMInicioPrincipal();
+        FR.setVisible(true);
+    }
+    
+}
